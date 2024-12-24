17135c01911ebe38467c4e6d747a88a9a87cf9d8
Mateusz Frąk <61707914+mfr-6@users.noreply.github.com>
2024-12-24 14:36:40 +0000

    basic unittests

--- backend/app/api/routes/products.py
+++ backend/app/api/routes/products.py
@@ -3,17 +3,33 @@ from fastapi import APIRouter
 router = APIRouter(prefix="/products", tags=["products"])
 
 @router.get("/")
-def read_products() -> list[dict[str, str]]:
-    return [{"name": "apple", "price": 1.0}, {"name": "banana", "price": 0.5}]
+def read_products() -> list[dict]:
+    return [
+        {
+            "name": "apple",
+            "price": 1.0
+        },
+        {
+            "name": "banana",
+            "price": 0.5
+        }
+    ]
 
 @router.get("/{product_id}")
-def read_product(product_id: int) -> dict[str, str]:
-    return {"name": "apple", "price": 1.0}
+def read_product(product_id: int) -> dict:
+    return {
+        "id": product_id,
+        "name": "apple",
+        "price": 1.0
+    }
 
-@router.post("/")
+@router.post("/", status_code=201)
 def create_product() -> dict[str, str]:
     return {"name": "created product"}
 
 @router.delete("/{product_id}")
-def delete_product(product_id: int) -> dict[str, str]:
-    return {"name": "deleted product"}
+def delete_product(product_id: int) -> dict:
+    return {
+        "id": product_id,
+        "name": "deleted product"
+    }
--- backend/app/tests/api/routes/test_products.py
+++ backend/app/tests/api/routes/test_products.py
@@ -1,21 +1,24 @@
+import random
 from fastapi.testclient import TestClient
 
 def test_read_products(test_client: TestClient) -> None:
-    response = test_client.get("/products/")
+    response = test_client.get("/api/v1/products/")
     assert response.status_code == 200
     assert response.json() == [{"name": "apple", "price": 1.0}, {"name": "banana", "price": 0.5}]
 
 def test_read_product(test_client: TestClient) -> None:
-    response = test_client.get("/products/1")
+    test_product_id = random.randint(1, 100)
+    response = test_client.get(f"/api/v1/products/{test_product_id}")
     assert response.status_code == 200
-    assert response.json() == {"name": "apple", "price": 1.0}
+    assert response.json() == {"id": test_product_id, "name": "apple", "price": 1.0}
 
 def test_create_product(test_client: TestClient) -> None:
-    response = test_client.post("/products/")
-    assert response.status_code == 200
+    response = test_client.post("/api/v1/products/")
+    assert response.status_code == 201
     assert response.json() == {"name": "created product"}
 
 def test_delete_product(test_client: TestClient) -> None:
-    response = test_client.delete("/products/1")
+    test_product_id = random.randint(1, 100)
+    response = test_client.delete(f"/api/v1/products/{test_product_id}")
     assert response.status_code == 200
-    assert response.json() == {"name": "deleted product"}
+    assert response.json() == {"id": test_product_id, "name": "deleted product"}
